5172862cf661af3b433e9c6e919ffce1e8ba81a6
itaya_A <itaya_a@gmail.com>
2017-11-14 07:35:10 +0900

    表示件数の変更

--- app/Controller/YoutubersController.php
+++ app/Controller/YoutubersController.php
@@ -5,8 +5,8 @@ App::uses('AppController', 'Controller');
 
 class YoutubersController extends AppController {
 	public function index(){
-        $this->set('youtubers_data', $this->Youtuber->find('all',['limit' => 3]));
+        $this->set('youtubers_data', $this->Youtuber->find('all',['limit' => 9]));
         $this->loadModel('Office');
-        $this->set('offices_data', $this->Office->find('all',['limit' => 3]));
+        $this->set('offices_data', $this->Office->find('all'));
 	}
 }
